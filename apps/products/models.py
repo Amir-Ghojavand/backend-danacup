0d629f86590474b4a8fca47b4a8fbb32b3996eab
Amir-Ghojavand <amir.ghojavand.16@gmail.com>
2025-05-21 09:48:15 +0330

    v1-dir

---
+++ apps/products/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    discount_percent = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='products/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    available = models.BooleanField(default=True)
+
+    def discounted_price(self):
+        return self.price * (100 - self.discount_percent) / 100
+
+    def __str__(self):
+        return self.name
